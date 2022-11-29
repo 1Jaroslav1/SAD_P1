bcb91cba6a5021ee7917a406af9b63617a44afc5
Yaroslav <garbar.jarek@gmail.com>
2022-11-29 18:32:32 +0100

    cleaned data :(

--- FilterData.R
+++ FilterData.R
@@ -1,10 +1,20 @@
 eu_inflation = read.csv("./EU_Inflation_HICP_data.csv")
-eu_inflation["Period.Unit"] = anytime::anydate(paste(eu_inflation[,"Period.Unit"], 1))
-eu_zone_countries = c("Period.Unit", "Austria","Belgium","Cyprus","Estonia","Finland","France","Germany","Greece","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta","Netherlands","Portugal","Slovakia","Slovenia","Spain")
-not_eu_zone_countries = colnames(eu_inflation)[!(colnames(eu_inflation) %in% eu_zone_countries) & colnames(eu_inflation) != "EU..changingcomposition."]
-not_eu_zone_countries = c(c("Period.Unit"), not_eu_zone_countries)
 
-eu_countries_in_zone_inflation = eu_inflation[eu_zone_countries]
-eu_countries_NOT_in_zone_inflation = eu_inflation[not_eu_zone_countries]
-pl_inflation = eu_inflation[c("Period.Unit", "Poland")]
-eu_changingcomposition_inflation = eu_inflation[c("Period.Unit", "EU..changingcomposition.")]
+colnames(eu_inflation)[1] = "Date"
+colnames(eu_inflation)[7] = "EU"
+
+eu_inflation = na.omit(eu_inflation)
+eu_inflation[, 2:29] = sapply(eu_inflation[, 2:29], as.double)
+eu_inflation["Date"] = anytime::anydate(paste(eu_inflation[,"Date"], 1))
+
+eu_inflation_tidy <- pivot_longer(eu_inflation, -Date, names_to = "Country", values_to = "Inflation")
+
+eu_zone_countries = c("Date", "Austria","Belgium","Cyprus","Estonia","Finland","France","Germany","Greece","Ireland","Italy","Latvia","Lithuania","Luxembourg","Malta","Netherlands","Portugal","Slovakia","Slovenia","Spain")
+not_eu_zone_countries = colnames(eu_inflation)[!(colnames(eu_inflation) %in% eu_zone_countries) & colnames(eu_inflation) != "EU..changingcomposition."]
+not_eu_zone_countries = c(c("Date"), not_eu_zone_countries)
+
+eu_countries_in_zone_inflation = filter(eu_inflation_tidy, Country %in% eu_zone_countries)
+eu_countries_NOT_in_zone_inflation = filter(eu_inflation_tidy, Country %in% not_eu_zone_countries)
+pl_inflation = filter(eu_inflation_tidy, Country == "Poland")
+eu_changingcomposition_inflation = filter(eu_inflation_tidy, Country == "EU")
+
--- Z1.1.R
+++ Z1.1.R
@@ -1,18 +1,46 @@
 # Libraries
 library(ggplot2)
 library(dplyr)
+library("tidyverse")
+library(tidyr)
+library(readr)
 
-pl_inflation = filter(pl_inflation, !is.na(pl_inflation["Poland"]))
-
-# Dummy data
-data = data.frame(
-  day = pl_inflation[,"Period.Unit"],
-  value = pl_inflation[,"Poland"]
+dataFrame = data.frame(
+  date = pl_inflation[,"Date"],
+  inflation = pl_inflation[,"Inflation"]
 )
 
-# Most basic bubble plot
-p = ggplot(data, aes(x=day, y=value)) +
-  geom_line() +
-  geom_point(shape=21, color="black", fill="#69b3a2", size=1.5) +
-  scale_x_date(date_labels = "%Y-%m", date_minor_breaks = "1 month")
-p
+dataFrame = filterDataByTime(dataFrame, c("2010-01-12", "2020-01-12"))
+
+
+
+drawData = function(dataFrame, lineColor, pointColor){
+  ggplot(dataFrame, aes(x=Date, y=Inflation)) +
+    geom_line(aes(color="Poland"), size=1) +
+    geom_point(shape=21, color=pointColor, fill=pointColor, size=1.5) +
+    scale_x_date(date_labels = "%Y-%m", date_minor_breaks = "1 month") +
+    scale_color_manual(values=lineColor)
+}
+
+drawSeveralData = function(data) {
+
+}
+
+filterDataByTime = function(data, period) {
+  return(data[(data$Date > period[1] & data$Inflation < period[2]), ])
+}
+
+filterDataByCountry = function() {
+
+}
+
+drawData(dataFrame, "Green", "#E14D2A")
+
+# time = eu_countries_NOT_in_zone_inflation[,"Period.Unit"]
+# value = eu_countries_NOT_in_zone_inflation %>% select(2:9)
+# group = colnames(eu_countries_NOT_in_zone_inflation)
+
+# data.frame(time, value, group)
+# multy_date =
+
+# drawSeveralData(eu_countries_NOT_in_zone_inflation)
