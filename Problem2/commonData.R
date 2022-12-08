fde0882a842f8670827468bcf0bc377effc1e61f
Yaroslav <garbar.jarek@gmail.com>
2022-12-09 00:03:55 +0100

    change inflation cart

--- Problem2/commonData.R
+++ Problem2/commonData.R
@@ -4,39 +4,40 @@ library(hash)
 market_basket = hash()
 
 # Milk
-market_basket[["Milk"]] <- 5
+market_basket[["Milk"]] <- 10
 market_basket[["Butter"]] <- 0.4
-market_basket[["Cheese"]] <- 0.3
+market_basket[["Cheese"]] <- 0.5
 market_basket[["Other Dairy Products & Eggs"]] <- 1
 
 # Vegetables
-market_basket[["Potatoes"]] <- 4.8
-market_basket[["Cabbage Vegetables"]] <- 2.3
+market_basket[["Potatoes"]] <- 6.3
+market_basket[["Cabbage Vegetables"]] <- 0.6
 market_basket[["Tomatoes"]] <- 1
 market_basket[["Root Vegetables & Mushrooms"]] <- 1
 market_basket[["Onions"]] <- 0.5
+market_basket[["Other Fresh Vegetables"]] <- 2
 
 # Fruits
 market_basket[["Apples & Pears"]] <- 1
-market_basket[["Bananas"]] <- 1
+market_basket[["Bananas"]] <- 0.6
 market_basket[["Citrus Fruits"]] <- 0.5
 market_basket[["Berries & Grapes"]] <- 0.5
 
 # Bread & Cereal Products
-market_basket[["Bread"]] <- 5
+market_basket[["Bread"]] <- 4
 market_basket[["Breakfast Cereals"]] <- 1
-market_basket[["Rice"]] <- 1
-market_basket[["Pasta"]] <- 1
+market_basket[["Rice"]] <- 0.7
+market_basket[["Pasta"]] <- 2
 
 # Meat
 market_basket[["Pork"]] <- 0.7
-market_basket[["Beef & Veal"]] <- 1.2
-market_basket[["Poultry"]] <- 1
+market_basket[["Beef & Veal"]] <- 0.6
+market_basket[["Poultry"]] <- 2
 market_basket[["Sausages"]] <- 1
 market_basket[["Ham & Bacon"]] <- 0.2
 
 # Fish
-market_basket[["Fresh Fish"]] <- 0.6
+market_basket[["Fresh Fish"]] <- 0.8
 market_basket[["Processed Fish & Seafood"]] <- 0.3
 
 # Other
